46509659c43a336bdc927b465880dbce3f2da304
Siddharth Mishra <smishra18@hawk.iit.edu>
2021-02-08 13:23:57 -0600

    assignment 2

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -84,14 +84,18 @@ class MoviesViewController: UIViewController,
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        print("Loading movie details")
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        tableView.deselectRow(at: indexPath, animated: true)
+            
+        
     }
-    */
+    
 
 }
